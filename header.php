1c48327c5f2ea82d24490d29a9a33e9210b50887
Bruno MC <bruno.br.220@hotmail.com>
2019-08-08 21:54:53 -0300

    Criado pagina de cadastro

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
+</body>
+</html>
